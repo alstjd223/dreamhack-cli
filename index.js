832d13900033a68417bc45341a113adfc6dceea5
alstjd223 <13hj45@gmail.com>
2025-04-17 08:13:27 +0900

    1

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wargame</title>
+    </head>
+    <body>
+      <div class="challenge">
+        <h1 class="challenge-info">||dir</h1>
+        <a href="https://sfo2.digitaloceanspaces.com/fake/path/wargame.zip" target="_blank" class="challenge-download">
+          Download
+        </a>
+      </div>
+    </body>
+    </html>
+  `;
+  res.send(html);
+});
+
+app.listen(PORT, () => {
+  console.log(`Fake Wargame site running at http://localhost:${PORT}`);
+});
